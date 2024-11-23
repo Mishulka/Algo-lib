fbc24c64b3dcdcd6c0033427d81813397e0e574e
Mishulka <misailgromov@gmail.com>
2024-11-23 18:09:41 +0300

    fix modules imports

---
+++ src/main.mjs
@@ -0,0 +1,14 @@
+// SORTING ARRAYS
+import { bubbleSort } from './sorting_algorithms/bubbleSort.mjs';
+import { fastSort } from './sorting_algorithms/fastSort.mjs';
+
+// MATHEMATICAL ALGORITHMS
+import { fastSort } from './math_algorithms/factorial.mjs';
+
+const algo = {
+  bubbleSort,
+  fastSort,
+  factorial,
+};
+
+export default algo;
