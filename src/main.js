6e00b378058b48cc2c878a4dcbf3897a11ab6f57
Mishulka <misailgromov@gmail.com>
2024-11-23 17:11:21 +0300

    add bubble and fast algorithms

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { bubbleSort } from './bubbleSort';
-import { fastSort } from './fastSort';
+// SORTING ARRAYS
+import { bubbleSort } from './sorting_algorithms/bubbleSort';
+import { fastSort } from './sorting_algorithms/fastSort';
 
 const algo = {
   bubbleSort,
