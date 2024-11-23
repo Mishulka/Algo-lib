23c8fde96c0a640bad435f3cd92a4b6a7ecf9a1d
Mishulka <misailgromov@gmail.com>
2024-11-23 19:38:09 +0300

    add 2 functions to algo

--- src/index.mjs
+++ src/index.mjs
@@ -4,11 +4,16 @@ import { fastSort } from './sorting_algorithms/fastSort.mjs';
 
 // MATHEMATICAL ALGORITHMS
 import { factorial } from './math_algorithms/factorial.mjs';
+import { fibonacci } from './math_algorithms/fibonacci.mjs';
+
+// WORDS ALGORITHMS
+import { isPalindrome } from './words_algorithms/palindrome.mjs';
 
 export const algo = {
   bubbleSort,
   fastSort,
   factorial,
+  isPalindrome,
 };
 
 
